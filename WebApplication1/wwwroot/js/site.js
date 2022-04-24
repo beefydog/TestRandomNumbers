eb7c3592da22716c1c38a30f2b84de3a745bbc51
beefydog <dave@beefydog.com>
2022-04-24 04:52:16 -0400

    test website added

---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,37 @@
+﻿
+$(document).ready(function () {
+    $('#btnget').click(function () {
+       GetData();
+    });
+
+    function GetData() {
+        let postdata = $('#postdata').val();
+     //   alert(postdata);
+
+        //https://api.miraclecat.com/api/numbersets
+        $.ajax({
+            url: "https://api.miraclecat.com/api/numbersets",
+            type: "post",
+            data: postdata,
+            dataType: 'text', //returns json when set to text - go figure, returns staggered array when set to json
+            contentType: "application/json",
+            success: function (result, status, xhr) {
+
+            /*    console.log(status);*/
+                console.log(result);
+
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr)
+            }
+
+
+
+        });
+
+
+    };
+
+
+});
+
