ebce2cdc3ae278a7bb41905651862f93acf2aca1
beefydog <dave@beefydog.com>
2022-05-08 01:53:20 -0400

    testing razor api consumption

---
+++ BlazorApp1/Models/NumberSetsRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp1.Models;
+
+public class NumberGroup
+{
+    public int min { get; set; }
+    public int max { get; set; }
+    public int numbersPerGroup { get; set; }
+    public int divergence { get; set; }
+    public bool sumCheck { get; set; }
+    public bool oeCheck { get; set; }
+}
+
+public class Root
+{
+    public List<NumberGroup> numberGroups { get; set; } = new List<NumberGroup>();
+    public int sets { get; set; }
+}
