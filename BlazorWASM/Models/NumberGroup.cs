f13529c41ceba0ec3e964469085373867cada633
beefydog <dave@beefydog.com>
2022-05-22 15:06:19 -0400

    added WASM project

---
+++ BlazorWASM/Models/NumberGroup.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorWASM.Models;
+
+public class NumberGroup
+{
+    public NumberGroup(int groupId, bool enabled, int minValue, int maxValue, int numbersPerGroup, int divergence, bool checkSumEnabled, bool checkOEEnabled)
+    {
+        GroupId = groupId;
+        Enabled = enabled;
+        MinValue = minValue;
+        MaxValue = maxValue;
+        NumbersPerGroup = numbersPerGroup;
+        Divergence = divergence;
+        CheckOEEnabled = checkOEEnabled;
+        CheckSumEnabled = checkSumEnabled;
+    }
+
+    public int GroupId { get; set; }
+
+    public bool Enabled { get; set; }
+
+    public int MinValue { get; set; }
+
+    public int MaxValue { get; set; }
+
+    public int NumbersPerGroup { get; set; }
+
+    public int Divergence { get; set; }
+
+    public bool CheckSumEnabled { get; set; }
+    public bool CheckOEEnabled { get; set; }
+
+}
---
+++ BlazorWASM/Models/NumberSetsRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorWASM.Models;
+
+public class NumberGroupRequest
+{
+    public int min { get; set; }
+    public int max { get; set; }
+    public int numbersPerGroup { get; set; }
+    public int divergence { get; set; }
+    public bool sumCheck { get; set; }
+    public bool oeCheck { get; set; }
+}
+
+public class Root
+{
+    public List<NumberGroupRequest> numberGroups { get; set; } = new List<NumberGroupRequest>();
+    public int sets { get; set; }
+}
+
