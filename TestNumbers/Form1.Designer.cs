0d228503992bccb4de968b28631ebef0a2e609a2
beefydog <dave@beefydog.com>
2024-07-03 00:48:34 -0400

    progress & gui changes

--- TestNumbers/Form1.Designer.cs
+++ TestNumbers/Form1.Designer.cs
@@ -28,195 +28,213 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-            this.btnStart = new System.Windows.Forms.Button();
-            this.txtBits = new System.Windows.Forms.TextBox();
-            this.txtResults = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtIterations = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtAverage = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtRange = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtMedian = new System.Windows.Forms.TextBox();
-            this.radCPURandom = new System.Windows.Forms.RadioButton();
-            this.radAlgoRandom = new System.Windows.Forms.RadioButton();
-            this.lblOther = new System.Windows.Forms.Label();
-            this.txtOther = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // btnStart
-            // 
-            this.btnStart.Location = new System.Drawing.Point(115, 60);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 26);
-            this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // txtBits
-            // 
-            this.txtBits.Location = new System.Drawing.Point(260, 60);
-            this.txtBits.MaxLength = 2;
-            this.txtBits.Name = "txtBits";
-            this.txtBits.Size = new System.Drawing.Size(47, 26);
-            this.txtBits.TabIndex = 1;
-            this.txtBits.Text = "8";
-            this.txtBits.Leave += new System.EventHandler(this.txtBits_Leave);
-            // 
-            // txtResults
-            // 
-            this.txtResults.Location = new System.Drawing.Point(115, 107);
-            this.txtResults.Multiline = true;
-            this.txtResults.Name = "txtResults";
-            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtResults.Size = new System.Drawing.Size(370, 258);
-            this.txtResults.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(220, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 19);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Bits:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(328, 64);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 19);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Iterations:";
-            // 
-            // txtIterations
-            // 
-            this.txtIterations.Location = new System.Drawing.Point(404, 61);
-            this.txtIterations.Name = "txtIterations";
-            this.txtIterations.Size = new System.Drawing.Size(81, 26);
-            this.txtIterations.TabIndex = 4;
-            this.txtIterations.Text = "500";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(495, 202);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 19);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Average:";
-            // 
-            // txtAverage
-            // 
-            this.txtAverage.Location = new System.Drawing.Point(563, 199);
-            this.txtAverage.Name = "txtAverage";
-            this.txtAverage.ReadOnly = true;
-            this.txtAverage.Size = new System.Drawing.Size(216, 26);
-            this.txtAverage.TabIndex = 6;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(512, 64);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 19);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Range:";
-            // 
-            // txtRange
-            // 
-            this.txtRange.Location = new System.Drawing.Point(563, 61);
-            this.txtRange.Name = "txtRange";
-            this.txtRange.ReadOnly = true;
-            this.txtRange.Size = new System.Drawing.Size(216, 26);
-            this.txtRange.TabIndex = 8;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(499, 255);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 19);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Median:";
-            // 
-            // txtMedian
-            // 
-            this.txtMedian.Location = new System.Drawing.Point(563, 252);
-            this.txtMedian.Name = "txtMedian";
-            this.txtMedian.ReadOnly = true;
-            this.txtMedian.Size = new System.Drawing.Size(216, 26);
-            this.txtMedian.TabIndex = 10;
-            // 
-            // radCPURandom
-            // 
-            this.radCPURandom.AutoSize = true;
-            this.radCPURandom.Checked = true;
-            this.radCPURandom.Location = new System.Drawing.Point(299, 16);
-            this.radCPURandom.Name = "radCPURandom";
-            this.radCPURandom.Size = new System.Drawing.Size(109, 23);
-            this.radCPURandom.TabIndex = 12;
-            this.radCPURandom.TabStop = true;
-            this.radCPURandom.Text = "CPU Random";
-            this.radCPURandom.UseVisualStyleBackColor = true;
-            // 
-            // radAlgoRandom
-            // 
-            this.radAlgoRandom.AutoSize = true;
-            this.radAlgoRandom.Location = new System.Drawing.Point(427, 16);
-            this.radAlgoRandom.Name = "radAlgoRandom";
-            this.radAlgoRandom.Size = new System.Drawing.Size(110, 23);
-            this.radAlgoRandom.TabIndex = 13;
-            this.radAlgoRandom.Text = "Algo Random";
-            this.radAlgoRandom.UseVisualStyleBackColor = true;
-            // 
-            // lblOther
-            // 
-            this.lblOther.AutoSize = true;
-            this.lblOther.Location = new System.Drawing.Point(563, 317);
-            this.lblOther.Name = "lblOther";
-            this.lblOther.Size = new System.Drawing.Size(48, 19);
-            this.lblOther.TabIndex = 15;
-            this.lblOther.Text = "Other:";
-            // 
-            // txtOther
-            // 
-            this.txtOther.Location = new System.Drawing.Point(563, 339);
-            this.txtOther.Name = "txtOther";
-            this.txtOther.ReadOnly = true;
-            this.txtOther.Size = new System.Drawing.Size(216, 26);
-            this.txtOther.TabIndex = 14;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 463);
-            this.Controls.Add(this.lblOther);
-            this.Controls.Add(this.txtOther);
-            this.Controls.Add(this.radAlgoRandom);
-            this.Controls.Add(this.radCPURandom);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.txtMedian);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtRange);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtAverage);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtIterations);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtResults);
-            this.Controls.Add(this.txtBits);
-            this.Controls.Add(this.btnStart);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+        btnStart = new Button();
+        txtBits = new TextBox();
+        txtResults = new TextBox();
+        label1 = new Label();
+        label2 = new Label();
+        txtIterations = new TextBox();
+        label3 = new Label();
+        txtAverage = new TextBox();
+        label4 = new Label();
+        txtRange = new TextBox();
+        label5 = new Label();
+        txtMedian = new TextBox();
+        radCPURandom = new RadioButton();
+        radAlgoRandom = new RadioButton();
+        lblOther = new Label();
+        txtOther = new TextBox();
+        progressBar1 = new ProgressBar();
+        SuspendLayout();
+        // 
+        // btnStart
+        // 
+        btnStart.Location = new Point(12, 47);
+        btnStart.Margin = new Padding(3, 2, 3, 2);
+        btnStart.Name = "btnStart";
+        btnStart.Size = new Size(101, 24);
+        btnStart.TabIndex = 0;
+        btnStart.Text = "Generate";
+        btnStart.UseVisualStyleBackColor = true;
+        btnStart.Click += button1_Click;
+        // 
+        // txtBits
+        // 
+        txtBits.Location = new Point(200, 47);
+        txtBits.Margin = new Padding(3, 2, 3, 2);
+        txtBits.MaxLength = 2;
+        txtBits.Name = "txtBits";
+        txtBits.Size = new Size(42, 23);
+        txtBits.TabIndex = 1;
+        txtBits.Text = "8";
+        txtBits.Leave += txtBits_Leave;
+        // 
+        // txtResults
+        // 
+        txtResults.Location = new Point(12, 84);
+        txtResults.Margin = new Padding(3, 2, 3, 2);
+        txtResults.Multiline = true;
+        txtResults.Name = "txtResults";
+        txtResults.ScrollBars = ScrollBars.Both;
+        txtResults.Size = new Size(402, 237);
+        txtResults.TabIndex = 2;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(165, 52);
+        label1.Name = "label1";
+        label1.Size = new Size(29, 15);
+        label1.TabIndex = 3;
+        label1.Text = "Bits:";
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(278, 52);
+        label2.Name = "label2";
+        label2.Size = new Size(59, 15);
+        label2.TabIndex = 5;
+        label2.Text = "Iterations:";
+        // 
+        // txtIterations
+        // 
+        txtIterations.Location = new Point(343, 48);
+        txtIterations.Margin = new Padding(3, 2, 3, 2);
+        txtIterations.Name = "txtIterations";
+        txtIterations.Size = new Size(71, 23);
+        txtIterations.TabIndex = 4;
+        txtIterations.Text = "500";
+        // 
+        // label3
+        // 
+        label3.AutoSize = true;
+        label3.Location = new Point(493, 108);
+        label3.Name = "label3";
+        label3.Size = new Size(53, 15);
+        label3.TabIndex = 7;
+        label3.Text = "Average:";
+        // 
+        // txtAverage
+        // 
+        txtAverage.Location = new Point(493, 125);
+        txtAverage.Margin = new Padding(3, 2, 3, 2);
+        txtAverage.Name = "txtAverage";
+        txtAverage.ReadOnly = true;
+        txtAverage.Size = new Size(195, 23);
+        txtAverage.TabIndex = 6;
+        // 
+        // label4
+        // 
+        label4.AutoSize = true;
+        label4.Location = new Point(444, 52);
+        label4.Name = "label4";
+        label4.Size = new Size(43, 15);
+        label4.TabIndex = 9;
+        label4.Text = "Range:";
+        // 
+        // txtRange
+        // 
+        txtRange.Location = new Point(493, 48);
+        txtRange.Margin = new Padding(3, 2, 3, 2);
+        txtRange.Name = "txtRange";
+        txtRange.ReadOnly = true;
+        txtRange.Size = new Size(195, 23);
+        txtRange.TabIndex = 8;
+        // 
+        // label5
+        // 
+        label5.AutoSize = true;
+        label5.Location = new Point(493, 182);
+        label5.Name = "label5";
+        label5.Size = new Size(50, 15);
+        label5.TabIndex = 11;
+        label5.Text = "Median:";
+        // 
+        // txtMedian
+        // 
+        txtMedian.Location = new Point(493, 199);
+        txtMedian.Margin = new Padding(3, 2, 3, 2);
+        txtMedian.Name = "txtMedian";
+        txtMedian.ReadOnly = true;
+        txtMedian.Size = new Size(195, 23);
+        txtMedian.TabIndex = 10;
+        // 
+        // radCPURandom
+        // 
+        radCPURandom.AutoSize = true;
+        radCPURandom.Checked = true;
+        radCPURandom.Location = new Point(262, 13);
+        radCPURandom.Margin = new Padding(3, 2, 3, 2);
+        radCPURandom.Name = "radCPURandom";
+        radCPURandom.Size = new Size(96, 19);
+        radCPURandom.TabIndex = 12;
+        radCPURandom.TabStop = true;
+        radCPURandom.Text = "CPU Random";
+        radCPURandom.UseVisualStyleBackColor = true;
+        // 
+        // radAlgoRandom
+        // 
+        radAlgoRandom.AutoSize = true;
+        radAlgoRandom.Location = new Point(374, 13);
+        radAlgoRandom.Margin = new Padding(3, 2, 3, 2);
+        radAlgoRandom.Name = "radAlgoRandom";
+        radAlgoRandom.Size = new Size(98, 19);
+        radAlgoRandom.TabIndex = 13;
+        radAlgoRandom.Text = "Algo Random";
+        radAlgoRandom.UseVisualStyleBackColor = true;
+        // 
+        // lblOther
+        // 
+        lblOther.AutoSize = true;
+        lblOther.Location = new Point(493, 250);
+        lblOther.Name = "lblOther";
+        lblOther.Size = new Size(0, 15);
+        lblOther.TabIndex = 15;
+        // 
+        // txtOther
+        // 
+        txtOther.Location = new Point(493, 268);
+        txtOther.Margin = new Padding(3, 2, 3, 2);
+        txtOther.Name = "txtOther";
+        txtOther.ReadOnly = true;
+        txtOther.Size = new Size(195, 23);
+        txtOther.TabIndex = 14;
+        // 
+        // progressBar1
+        // 
+        progressBar1.Location = new Point(12, 339);
+        progressBar1.Name = "progressBar1";
+        progressBar1.Size = new Size(676, 23);
+        progressBar1.TabIndex = 16;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(700, 366);
+        Controls.Add(progressBar1);
+        Controls.Add(lblOther);
+        Controls.Add(txtOther);
+        Controls.Add(radAlgoRandom);
+        Controls.Add(radCPURandom);
+        Controls.Add(label5);
+        Controls.Add(txtMedian);
+        Controls.Add(label4);
+        Controls.Add(txtRange);
+        Controls.Add(label3);
+        Controls.Add(txtAverage);
+        Controls.Add(label2);
+        Controls.Add(txtIterations);
+        Controls.Add(label1);
+        Controls.Add(txtResults);
+        Controls.Add(txtBits);
+        Controls.Add(btnStart);
+        Margin = new Padding(3, 2, 3, 2);
+        Name = "Form1";
+        Text = "CPU Random vs ALGO Random";
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
@@ -237,4 +255,5 @@ partial class Form1
     private RadioButton radAlgoRandom;
     private Label lblOther;
     private TextBox txtOther;
+    private ProgressBar progressBar1;
 }
--- TestNumbers/Form1.cs
+++ TestNumbers/Form1.cs
@@ -1,167 +1,159 @@
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Text;
-
 using MathNet.Numerics.Statistics;
-namespace TestNumbers;
 
-
-public partial class Form1 : Form
+namespace TestNumbers
 {
-    public ushort bits { get; set; } = 8;
-
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
-        ComputeRange(bits);
-    }
+        public ushort bits { get; set; } = 8;
 
-    private async void button1_Click(object sender, EventArgs e)
-    {
-        bits = ushort.Parse(txtBits.Text.Trim());
-        int iterations = int.Parse(txtIterations.Text.Trim());
-        if (radCPURandom.Checked)
+        public Form1()
         {
-            DisplayResults(await Task.Run(() => CPURandom(iterations, bits)));
-        }
-        else
-        {
-            DisplayResults(await Task.Run(() => AlgoRandom(iterations, bits)));
-        }
-    }
-
-    /// <summary>
-    /// Due to limitations in the Statistics library (which uses double), any list of numbers having powers greater than 2^53 will be
-    /// computed inaccurately for the Mean, Median, and PopulationSkewness. This is a mantissa limitation with double datatype.
-    /// </summary>
-    /// <param name="results"></param>
-    private void DisplayResults(Results results)
-    {
-        IEnumerable<double> nums = (IEnumerable<double>)(results.Numbers);
-        txtResults.Text = results.TextResults;
-        txtAverage.Text = Statistics.Mean(nums).ToString("N2");
-        txtMedian.Text = Statistics.Median(nums).ToString("N2");
-        txtOther.Text = Statistics.PopulationSkewness(nums).ToString("N3");
-        lblOther.Text = "PopulationSkewness:";
-    }
-
-    /// <summary>
-    /// Generates random numbers via the algorithm. a.k.a. pseudo-random
-    /// Utilized BigIntegers to be able to compute random numbers up to 2^64 -1 
-    /// The rnd.NextInt64 only works with signed longs, not ulongs, so 2^63-1 is the highest positive
-    /// </summary>
-    /// <param name="iterations"></param>
-    /// <param name="bits"></param>
-    /// <returns></returns>
-    public static Results AlgoRandom(int iterations, ushort bits)
-    {
-        List<double> Numbers = new();
-        if (bits > 64) bits = 64; //hard limit to 64bit
-
-        BigInteger HighNumber = (BigInteger.One << bits) - 1;
-
-        StringBuilder sbResults = new();
-        Random rnd = new Random();
-
-        for (int j = 0; j < iterations; j++)
-        {
-            BigInteger RandomNumber = GetRandomBigInteger(rnd, bits);
-            Numbers.Add((double)RandomNumber);
-            sbResults.AppendLine(RandomNumber.ToString());
+            InitializeComponent();
+            ComputeRange(bits);
         }
 
-        return new() { Numbers = Numbers, TextResults = sbResults.ToString() };
-    }
-
-    /// <summary>
-    /// Helper function for AlgoRandom uses rnd.NextBytes instead 
-    /// </summary>
-    /// <param name="rnd"></param>
-    /// <param name="bits"></param>
-    /// <returns></returns>
-    private static BigInteger GetRandomBigInteger(Random rnd, int bits)
-    {
-        byte[] bytes = new byte[(bits + 7) / 8]; // Number of bytes needed
-        rnd.NextBytes(bytes);
-
-        BigInteger result = new BigInteger(bytes);
-        return result & ((BigInteger.One << bits) - 1); // Mask to ensure the correct number of bits
-    }
-
-
-    /// <summary>
-    /// Generates random numbers from CPU Noise - no algorithm, more cores = more randomness
-    /// utilizes GenerateBoolean for random binaries, then assembles into string
-    /// </summary>
-    /// <param name="iterations"></param>
-    /// <param name="bits"></param>
-    /// <returns></returns>
-    public static Results CPURandom(int iterations, ushort bits)
-    {
-        List<double> Numbers = new();
-        StringBuilder sbResults = new();
-
-        for (int j = 0; j < iterations; j++)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            StringBuilder sb = new();
-            for (ushort i = 0; i < bits; i++)
+            ToggleButtons();
+            ResetForm();
+            bits = ushort.Parse(txtBits.Text.Trim());
+            int iterations = int.Parse(txtIterations.Text.Trim());
+
+            var progress = new Progress<int>(value => progressBar1.Value = value);
+            progressBar1.Maximum = iterations;
+            progressBar1.Value = 0;
+
+            if (radCPURandom.Checked)
             {
-                sb.Append(GenerateBoolean() ? '1' : '0');
+                DisplayResults(await Task.Run(() => CPURandom(iterations, bits, progress)));
             }
-            string numberStr = sb.ToString();
-            if (GenerateBoolean()) numberStr.Reverse();
-
-            UInt64 number = Convert.ToUInt64(numberStr, 2);
-            Numbers.Add(Convert.ToDouble(number));
-            sbResults.AppendLine(number.ToString());
+            else
+            {
+                DisplayResults(await Task.Run(() => AlgoRandom(iterations, bits, progress)));
+            }
+            ToggleButtons();
         }
 
-        return new() { Numbers = Numbers, TextResults = sbResults.ToString() };
-    }
-
-    /// <summary>
-    /// Helper function for CPURandom 
-    /// This is the secret sauce for generating random bits via threading interlocks (which are random in nature)
-    /// </summary>
-    /// <returns></returns>
-    public static bool GenerateBoolean()
-    {
-        UInt64 gen1 = 0;
-        UInt64 gen2 = 0;
-        Task.Run(() =>
-         {
-             while (gen1 < 1 || gen2 < 1) Interlocked.Increment(ref gen1);
-         });
-        while (gen1 < 1 || gen2 < 1) Interlocked.Increment(ref gen2);
-        return (gen1 + gen2) % 2 == 0;
-    }
-
-    private void ComputeRange(ushort bits)
-    {
-        UInt64 topOfRange;
-        if (bits > 63)
+        private void DisplayResults(Results results)
         {
-            topOfRange = ulong.MaxValue;
-            bits = 64;
+            IEnumerable<double> nums = results.Numbers;
+            txtResults.Text = results.TextResults;
+            txtAverage.Text = Statistics.Mean(nums).ToString("N2");
+            txtMedian.Text = Statistics.Median(nums).ToString("N2");
+            txtOther.Text = Statistics.PopulationSkewness(nums).ToString("N3");
+            lblOther.Text = "PopulationSkewness:";
         }
-        else
+
+        public static Results AlgoRandom(int iterations, ushort bits, IProgress<int> progress)
         {
-            topOfRange = (1UL << bits) - 1UL; //instead of Math.Pow, I use the bitwise left shift operator
+            List<double> Numbers = new();
+            if (bits > 64) bits = 64;
+
+            BigInteger HighNumber = (BigInteger.One << bits) - 1;
+
+            StringBuilder sbResults = new();
+            Random rnd = new Random();
+
+            for (int j = 0; j < iterations; j++)
+            {
+                BigInteger RandomNumber = GetRandomBigInteger(rnd, bits);
+                Numbers.Add((double)RandomNumber);
+                sbResults.AppendLine(RandomNumber.ToString());
+                progress.Report(j + 1); 
+            }
+
+            return new() { Numbers = Numbers, TextResults = sbResults.ToString() };
         }
 
-        txtRange.Text = "0 - " + topOfRange.ToString();
-        if (bits == 64) txtBits.Text = "64";
+        private static BigInteger GetRandomBigInteger(Random rnd, int bits)
+        {
+            byte[] bytes = new byte[(bits + 7) / 8]; // Number of bytes needed
+            rnd.NextBytes(bytes);
+
+            BigInteger result = new BigInteger(bytes);
+            return result & ((BigInteger.One << bits) - 1); // Mask to ensure the correct number of bits
+        }
+
+        public static Results CPURandom(int iterations, ushort bits, IProgress<int> progress)
+        {
+            List<double> Numbers = new();
+            StringBuilder sbResults = new();
+
+            for (int j = 0; j < iterations; j++)
+            {
+                StringBuilder sb = new();
+                for (ushort i = 0; i < bits; i++)
+                {
+                    sb.Append(GenerateBoolean() ? '1' : '0');
+                }
+                string numberStr = sb.ToString();
+                if (GenerateBoolean()) numberStr.Reverse();
+
+                UInt64 number = Convert.ToUInt64(numberStr, 2);
+                Numbers.Add(Convert.ToDouble(number));
+                sbResults.AppendLine(number.ToString());
+                progress.Report(j + 1); 
+            }
+
+            return new() { Numbers = Numbers, TextResults = sbResults.ToString() };
+        }
+
+        public static bool GenerateBoolean()
+        {
+            int gen1 = 0;
+            int gen2 = 0;
+            Task.Run(() =>
+            {
+                while (gen1 < 1 || gen2 < 1) Interlocked.Increment(ref gen1);
+            });
+            while (gen1 < 1 || gen2 < 1) Interlocked.Increment(ref gen2);
+            return (gen1 + gen2) % 2 == 0;
+        }
+
+        private void ComputeRange(ushort bits)
+        {
+            UInt64 topOfRange;
+            if (bits > 63)
+            {
+                topOfRange = ulong.MaxValue;
+                bits = 64;
+            }
+            else
+            {
+                topOfRange = (1UL << bits) - 1UL;
+            }
+
+            txtRange.Text = "0 - " + topOfRange.ToString();
+            if (bits == 64) txtBits.Text = "64";
+        }
+
+        private void txtBits_Leave(object sender, EventArgs e)
+        {
+            bits = ushort.Parse(txtBits.Text.Trim());
+            ComputeRange(bits);
+        }
+
+        private void ResetForm()
+        {
+            txtResults.Text = string.Empty;
+            txtAverage.Text = string.Empty;
+            txtMedian.Text = string.Empty;
+            txtOther.Text = string.Empty;   
+            lblOther.Text = string.Empty;           
+        }
+
+        private void ToggleButtons()
+        {
+            btnStart.Enabled = !btnStart.Enabled;
+        }
     }
 
-    private void txtBits_Leave(object sender, EventArgs e)
+    public class Results
     {
-        bits = ushort.Parse(txtBits.Text.Trim());
-        ComputeRange(bits);
+        public List<double>? Numbers { get; set; }
+        public string? TextResults { get; set; }
     }
-}
-
-public class Results
-{
-    public List<double>? Numbers { get; set; }
-    public string? TextResults { get; set; }
 }
